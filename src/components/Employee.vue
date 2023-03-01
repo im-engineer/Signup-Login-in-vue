f31cbb187218b6f3aceb6738e8489efd77199c48
im-engineer <azmsiddhant1@gmail.com>
2023-03-01 18:00:58 +0530

    1st commit on 1 March 2023

---
+++ src/components/Employee.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Employee List</h1>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(axios,VueAxios)
+export default{
+        name : "Employee List",
+        mounted(){
+            Vue.axios.get("https://dummy.restapiexample.com/api/v1/employees").then(response => {
+                console.warn(response)
+            })
+        }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1 v-if="error.length">
+       <div>
+        <b>Please correct the following errors</b>
+        <ul>
+            <li v-for="e in error" v-bind:key="e.id">  {{ e }}</li>
+        </ul>
+       </div>
+    </h1>
+    <form @submit="login">
+      <h4>Login Form</h4>
+      <input
+        type="username"
+        name="username"
+        placeholder="Enter username"
+        v-model="loginData.username"
+      /><br /><br />
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter password"
+        v-model="loginData.password"
+      /><br /><br />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+  data() {
+    return {
+        error : [],
+        loginData : {
+        username: null,
+        password: null,
+        }
+        };
+  },
+  methods: {
+    login(e) {
+        this.error = [];
+        if(this.loginData.username && this.loginData.password){
+            console.warn("no error")
+        }
+        if(!this.loginData.username){
+            this.error.push("username is required")
+        }
+        if(!this.loginData.password){
+            this.error.push("username is password")
+        }
+        console.log("hello", this.error)
+         e.preventDefault();
+    },
+  },
+};
+</script>
